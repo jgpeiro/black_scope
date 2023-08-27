74171de0560c4d79d66d570491c89d259a578051
jgpeiro <jose@iptronix.com>
2023-08-27 22:50:24 +0200

    - Improve TSC. Now it works perfect.

--- software/stm32g4_scope/Core/Src/app_freertos.c
+++ software/stm32g4_scope/Core/Src/app_freertos.c
@@ -1344,7 +1344,7 @@ void _StartTaskTsc(void *argument)
 /* USER CODE BEGIN Application */
 void StartTaskTsc(void *argument)
 {
-	const TickType_t xFrequency = 10;
+	const TickType_t xFrequency = 1;
 	TickType_t xLastWakeTime;
 
 	struct sQueueTscUi msgTscUi = {0};
@@ -1364,12 +1364,67 @@ void StartTaskTsc(void *argument)
 	);
 
 	xLastWakeTime = xTaskGetTickCount();
+	uint16_t x;
+	uint16_t y;
+	uint16_t x_low = 0;
+	uint16_t y_low = 0;
+	int16_t cnt = 0;
+	int released_sent = 0;
+	int cnt2 = 0;
 	for(;;)
   	{
 		vTaskDelayUntil( &xLastWakeTime, xFrequency );
-		tsc_read( &tsc, &msgTscUi.p, &msgTscUi.x, &msgTscUi.y );
-        osMessageQueuePut( queueTscUiHandle, &msgTscUi, 0U, 0U );
-	}
+		//tsc_read( &tsc, &msgTscUi.p, &msgTscUi.x, &msgTscUi.y );
+		tsc_read_ll( &tsc, &x, &y );
+		if( x )
+		{
+			if( cnt < 50 )
+			{
+				cnt += 1;
+			}
+		}
+		else
+		{
+			cnt = 0;
+		}
+		x_low = x_low*0.9 + x*0.1;
+		y_low = y_low*0.9 + y*0.1;
+
+		if( x )
+		{
+
+		}
+		else
+		{
+			x_low = 0;
+			y_low = 0;
+		}
+
+		msgTscUi.x = tsc.ax * x_low + tsc.bx;
+		msgTscUi.y = tsc.ay * y_low + tsc.by;
+		msgTscUi.p = x && (cnt>40);
+		if( x )
+		{
+			if( cnt2 < 10 )
+			{
+				cnt2 += 1;
+			}
+			else
+			{
+				cnt2 = 0;
+				osMessageQueuePut( queueTscUiHandle, &msgTscUi, 0U, 0U );
+			}
+			released_sent = 0;
+		}
+		else
+		{
+			if( released_sent == 0 )
+			{
+				osMessageQueuePut( queueTscUiHandle, &msgTscUi, 0U, portMAX_DELAY );
+				released_sent = 1;
+			}
+		}
+  	}
 }
 
 #define LCD_COLOR_WHITE		(0xFFFF)
@@ -2122,9 +2177,22 @@ void StartTaskScope(void *argument)
 
         if( running )
         {
+        	extern int DMA1_Channel1_CNDTR;
+        	int a;
+        	static int b = 0;
+        	a = DMA1_Channel1_CNDTR;
         	_scope_start( &scope, 0 );
             int result = _scope_wait( &scope, xFrequency );
-        	_scope_stop( &scope );
+        	if( a != b )
+        	{
+        		result = 1;
+        	}
+        	else
+        	{
+        		result = 0;
+        	}
+        	b = a;
+            _scope_stop( &scope );
 			if( osSemaphoreAcquire( semaphoreLcdHandle, portMAX_DELAY ) == osOK )
 			{
 				if( result )
